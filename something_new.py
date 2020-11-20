09bf00558062f5b454c2e324707a1029049b300a
balazskosa <71549252+balazskosa@users.noreply.github.com>
2020-11-20 18:27:11 +0100

    release

--- something_new.py
+++ something_new.py
@@ -1,2 +1,9 @@
-print("I'm hungry")
-print("No, i am thirsty now")
+import random
+
+print("What Does the Cat Say?")
+
+possible_answer = ["szeretlek te csöves"] * 18
+possible_answer.append("hm, homok, beleszarok")
+possible_answer.append("hm, hal, felfalom")
+
+print(possible_answer[random.randrange(20)])
