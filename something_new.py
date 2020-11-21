2d31d8a49f58bbdbeaace3b87db4bbd58a3cce99
balazskosa <71549252+balazskosa@users.noreply.github.com>
2020-11-21 21:24:01 +0100

    ....

--- something_new.py
+++ something_new.py
@@ -2,8 +2,3 @@ import random
 
 print("What Does the Cat Say?")
 
-possible_answer = ["szeretlek te csöves"] * 18
-possible_answer.append("hm, homok, beleszarok")
-possible_answer.append("hm, hal, felfalom")
-
-print(possible_answer[random.randrange(20)])
